6933344a48e64f6e0ba3665ad81370cedbeca06f
Andhika Rahman <andhikarhmnp@gmail.com>
2026-06-20 10:54:12 +0700

    create basic dp implementation

--- competitive-1/dp/b.cpp
+++ competitive-1/dp/b.cpp
@@ -2,28 +2,34 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	int n, k;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-	cin >> n >> k;
+  int n, k;
 
-	vector<int> w(k), h(k);
+  cin >> n >> k;
 
-	for(int i = 0; i < k; i++) cin >> w[i] >> h[i];
+  vector<int> w(k), h(k);
 
-	vector<int> first(n + 1);
+  for(int i = 0; i < k; i++) cin >> w[i] >> h[i];
 
-	vector<vector<int>> dp(k + 1, vector<int>(n + 1));
+  vector<vector<int>> dp(k + 1, vector<int>(n + 1));
 
-	for(int c = 0; c <= n; c++) dp[0][c] = 0;
+  for(int c = 0; c <= n; c++) dp[0][c] = 0;
 
-	for(int i = 1; i <= k; i++) {
-		for(int c = 0; c <= n; c++) {
-			int best;
-		}
-	}
-	
-	return 0;
+  for(int i = 1; i <= k; i++) {
+    for(int c = 0; c <= n; c++) {
+      int best = dp[i - 1][c];
+
+      if(w[i - 1] <= c && best < dp[i - 1][c - w[i - 1]] + h[i - 1]) {
+        best = dp[i - 1][c - w[i - 1]] + h[i - 1];
+      }
+
+      dp[i][c] = best;
+    }
+  }
+
+  cout << dp[k][n];
+
+  return 0;
 }
