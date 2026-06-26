27d3a440a63398656ca7840323ef7b7fa82ffa48
Andhika Rahman <andhikarhmnp@gmail.com>
2026-06-27 06:13:54 +0700

    solve another one brute force problem

--- drill/problems/brute_force/string_cantik.cpp
+++ drill/problems/brute_force/string_cantik.cpp
@@ -33,25 +33,19 @@ int main() {
     int best = -1;
 
     do {
-        int last_idx;
+        char prev = first[2];
 
-        for(int i = 0; i <= s.size() - 3; i++) {
-            if(s[i] == first[0] && s[i + 1] == first[1] && s[i + 2] == first[2]) {
-                ans += 3;
-                i += 3;
-                last_idx = i;
-            }
-        }
-
-        int i = s.size() - 2;
-
-        if(s[i] == first[0]) {
-            ans++;
-            if(s[i + 1] == first[1]) {
+        for(auto c : s) {
+            if(prev == first[2] && c == first[0]) {
                 ans++;
+                prev = c;
+            } else if(prev == first[1] && c == first[2]) {
+                ans++;
+                prev = c;
+            } else if(prev == first[0] && c == first[1]) {
+                ans++;
+                prev = c;
             }
-        } else if(s[i + 1] == first[0]) {
-            ans++;
         }
 
         best = std::max(best, ans);
