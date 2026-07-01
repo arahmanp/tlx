a93c634c98e3b8ca4a5479b06538430761ec7091
Andhika Rahman <andhikarhmnp@gmail.com>
2026-07-01 17:03:54 +0700

    solve tlx osn 2010 2d implementation problem

--- drill/problems/implementation/osn2010_2d.cpp
+++ drill/problems/implementation/osn2010_2d.cpp
@@ -1,1 +1,32 @@
 // https://tlx.toki.id/problems/osn-2010/2D
+
+#include <ios>
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n, d;
+
+    std::cin >> n >> d;
+
+    int curr_dna;
+    std::cin >> curr_dna;
+
+    int ans = 1;
+
+    int prev_dna = curr_dna;
+
+    for(int i = 1; i < n; i++) {
+        std::cin >> curr_dna;
+
+        if(curr_dna - prev_dna >= d) ans++;
+
+        prev_dna = curr_dna;
+    }
+
+    std::cout << ans;
+
+    return 0;
+}
